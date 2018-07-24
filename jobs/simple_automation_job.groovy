4697649ed0e284614d4b11b3d5bb83885002c710
Alexander Vynnyk <oleksandr.vynnyk@thomascookonline.com>
2018-07-24 18:15:51 +0300

    dev

--- jobs/simple_automation_job.groovy
+++ jobs/simple_automation_job.groovy
@@ -42,23 +42,3 @@ node('linux') {
         // npm_run_args = 'fe:build';
     }
 }
-// node('linux') {
-
-//     git credentialsId: 'b3cae613-c8f3-4f12-bcd5-75988c058d9a', url: 'https://github.com/ThomasCookOnline/DigitalAutomationTesting.git'
-
-//     docker.image('node:8').inside("-u 1000:996") {
-
-//         // stage('motherfucker') {
-//         sh '''
-//         set -x
-
-//     npm prune
-//     npm install
-//     rm -rf ${allure_path}
-//     node run ${args}
-//     '''
-
-//         // allure includeProperties: false, jdk: '', results: [[path: '${allure_path}']]
-//         // }
-//     }
-// }
