1ed2f40f8b361eb4ad92537773bad0fb467e07ae
Alexander Vynnyk <oleksandr.vynnyk@thomascookonline.com>
2018-07-24 16:26:21 +0300

    dev

--- jobs/simple_automation_job.groovy
+++ jobs/simple_automation_job.groovy
@@ -1,15 +1,23 @@
 import hudson.FilePath;
 
-runPipeline('githubflow') {
-  appName = 'DigitalAutomationTesting'
-  platform = 'nodejs'
-  baseContainerVersion = '8.9.4'
-  // role = 'sponsorship-api'
-  // cookbookName = 'tc-sponsorship-api'
-  // notificationEmail = 'panthers@thomascookonline.com'
-  // npm_run_args = 'fe:build';
-}
+node('linux') {
+    library identifier: 'jenkins-libs',
+        retriever: modernSCM(github(credentialsId: 'b3cae613-c8f3-4f12-bcd5-75988c058d9a',
+                                    id: '845e0207-1837-425f-b354-fe99baf714c1',
+                                    repoOwner: 'ThomasCookOnline',
+                                    repository: 'jenkins-libs',
+                                    traits: [headRegexFilter('TESTAUTO-1532-flow-for-test-automation')]))
 
+    runPipeline('githubflow') {
+        appName = 'DigitalAutomationTesting'
+        platform = 'nodejs'
+        baseContainerVersion = '8.9.4'
+        // role = 'sponsorship-api'
+        // cookbookName = 'tc-sponsorship-api'
+        // notificationEmail = 'panthers@thomascookonline.com'
+        // npm_run_args = 'fe:build';
+    }
+}
 // node('linux') {
 
 //     git credentialsId: 'b3cae613-c8f3-4f12-bcd5-75988c058d9a', url: 'https://github.com/ThomasCookOnline/DigitalAutomationTesting.git'
