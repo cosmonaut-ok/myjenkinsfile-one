dd2f5a2179f7cab2e0cb182e94d5b9ddd73d7a3d
Alexander Vynnyk <oleksandr.vynnyk@thomascookonline.com>
2018-07-24 16:04:25 +0300

    dev

--- jobs/simple_automation_job.groovy
+++ jobs/simple_automation_job.groovy
@@ -1,22 +1,32 @@
 import hudson.FilePath;
 
-node('linux') {
-
-    git credentialsId: 'b3cae613-c8f3-4f12-bcd5-75988c058d9a', url: 'https://github.com/ThomasCookOnline/DigitalAutomationTesting.git'
-
-    docker.image('node:8').inside("-u 1000:996") {
-
-        // stage('motherfucker') {
-        sh '''
-        set -x
-
-    npm prune
-    npm install
-    rm -rf ${allure_path}
-    node run ${args}
-    '''
-
-        // allure includeProperties: false, jdk: '', results: [[path: '${allure_path}']]
-        // }
-    }
+runPipeline('githubflow') {
+  appName = 'DigitalAutomationTesting'
+  platform = 'nodejs'
+  baseContainerVersion = '8.9.4'
+  // role = 'sponsorship-api'
+  // cookbookName = 'tc-sponsorship-api'
+  // notificationEmail = 'panthers@thomascookonline.com'
+  // npm_run_args = 'fe:build';
 }
+
+// node('linux') {
+
+//     git credentialsId: 'b3cae613-c8f3-4f12-bcd5-75988c058d9a', url: 'https://github.com/ThomasCookOnline/DigitalAutomationTesting.git'
+
+//     docker.image('node:8').inside("-u 1000:996") {
+
+//         // stage('motherfucker') {
+//         sh '''
+//         set -x
+
+//     npm prune
+//     npm install
+//     rm -rf ${allure_path}
+//     node run ${args}
+//     '''
+
+//         // allure includeProperties: false, jdk: '', results: [[path: '${allure_path}']]
+//         // }
+//     }
+// }
